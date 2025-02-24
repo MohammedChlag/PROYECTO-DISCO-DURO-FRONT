0a5764fee18da874406ae0def2cf38b1f0caa433
Mohammed-Chikri <mohammedchikrilag@icloud.com>
2025-02-24 20:36:11 +0100

    Mas estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { LayoutPage } from './pages/LayoutPage.jsx';
+import { LandingPage } from './pages/LandingPage.jsx';
+import { HomePage } from './pages/HomePage.jsx';
+import { RegisterPage } from './pages/RegisterPage.jsx';
+import { ValidationPage } from './pages/ValidationPage.jsx';
+import { LoginPage } from './pages/LoginPage.jsx';
 
 function App() {
-  return <></>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<LayoutPage />}>
+                    <Route index element={<LandingPage />} />
+                    <Route path="/storage" element={<HomePage />} />
+                    <Route path="/users/register" element={<RegisterPage />} />
+                    <Route
+                        path="/users/validation"
+                        element={<ValidationPage />}
+                    />
+                    <Route path="/users/login" element={<LoginPage />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
