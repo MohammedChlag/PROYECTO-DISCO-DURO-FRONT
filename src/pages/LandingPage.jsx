699af3235abdf76ec158f15318b7b6b51d29a692
Mohammed-Chikri <mohammedchikrilag@icloud.com>
2025-02-25 19:14:49 +0100

    Import Delete

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,4 @@
 import { LayoutComun } from '../components/LayoutComun1/LayoutComun.jsx';
-import { LayoutComun } from '../components/LayoutComun/LayoutComun.jsx';
-import { Button } from '../components/Button.jsx';
 
 export const LandingPage = () => {
     return (
