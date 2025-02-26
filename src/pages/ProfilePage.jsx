90752d868abb716062f5bc6b845173c4e11d0ce3
javi.garrido <fjgarridovalderas@hotmail.com>
2025-02-26 22:02:17 +0100

    Profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
-
 export const ProfilePage = () => {
-    return <div>ProfilePage</div>;
+    return (
+        <LayoutPrivate>
+            <h2>Username</h2>
+            <section>
+                <p>Editar perfil</p>
+            </section>
+            <section>
+                <img src="" alt="" />
+                <button>Editar avatar</button>
+                <div>
+                    <p>Nombre de usuario</p>
+                    <p>Username</p>
+                    <p>Nombre</p>
+                    <p>Nombre</p>
+                    <p>Apellido</p>
+                    <p>Apellido</p>
+                    <p>Email</p>
+                    <p>Email</p>
+                    <button>Guardar</button>
+                </div>
+            </section>
+            <section>Cambiar contraseña</section>
+            <section>
+                <p>Contraseña</p>
+                <p>Contraseña actual</p>
+                <p>Nueva contraseña</p>
+                <p>Nueva contraseña</p>
+                <p>Confirmar contraseña</p>
+                <p>Confirmar contraseña</p>
+            </section>
+        </LayoutPrivate>
+    );
 };
