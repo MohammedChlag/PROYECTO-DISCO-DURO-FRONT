ba7073b9509368980db51ce1b51d65988cecebf5
javi.garrido <fjgarridovalderas@hotmail.com>
2025-02-27 12:51:09 +0100

    Arreglo Footer

--- src/components/LayoutPublic/Footer/Footer.jsx
+++ src/components/LayoutPublic/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import logo from '../../../assets/img/logo-hackloud-solo.png';
 
 export const Footer = () => {
     return (
-        <footer className="flex-col justify-items-center w-full h-28 bg-gradient-to-r from-cyan-300 to-slate-900 bottom-0">
-            <img className="" src={logo} alt="Logo Hackloud" />
+        <footer className="flex flex-col items-center justify-center w-full h-28 bg-gradient-to-r from-cyan-300 to-slate-900 bottom-0">
+            <img className="w-32" src={logo} alt="Logo Hackloud" />
             <p className="text-sm mt-8">Copyright© Hackloud</p>
         </footer>
     );
