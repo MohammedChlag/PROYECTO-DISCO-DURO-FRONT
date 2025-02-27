54f1de972659e64c6f582c38fbdc39ed9aa7900d
Mohammed-Chikri <mohammedchikrilag@icloud.com>
2025-02-27 15:40:59 +0100

    Esto ya esta cogiendo forma...

--- src/components/LayoutPublic/Footer.jsx
+++ src/components/LayoutPublic/Footer.jsx
@@ -2,9 +2,11 @@ import { Logo } from '../Logo.jsx';
 
 export const Footer = () => {
     return (
-        <footer className="flex-col justify-items-center w-full h-28 bg-gradient-to-r from-cyan-300 to-slate-900 bottom-0">
-            <Logo />
-            <p className="text-sm mt-8">Copyright© Hackloud</p>
+        <footer className="bg-gradient-to-r from-cyan-300 to-slate-900 py-4">
+            <div className="flex flex-col items-center">
+                <Logo />
+                <p className="text-white/90 text-sm mt-4">Copyright Hackloud</p>
+            </div>
         </footer>
     );
 };
