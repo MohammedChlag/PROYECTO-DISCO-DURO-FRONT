1e33c1aa41fc4865cef1f02b2325bb6c71916827
javi.garrido <fjgarridovalderas@hotmail.com>
2025-03-02 19:18:40 +0100

    Responsive Landing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ export const LandingPage = () => {
         <>
             <section className=" flex flex-col items-center justify-center text-center text-[#009EB5]">
                 <img
-                    className="mt-24 w-32 md:mt-24 md:w-40"
+                    className="mt-20 w-32 md:mt-22 md:w-40"
                     src={nubelogo}
                     alt="Nube Informática"
                 />
@@ -20,19 +20,19 @@ export const LandingPage = () => {
                 </p>
             </section>
 
-            <section className=" py-4 text-center">
-                <h3 className="text-sm text-black mb-4 px-4 font-medium">
+            <section className="py-4 pt-6 text-center">
+                <h3 className="text-sm text-black px-4 pt-8 pb-3 font-medium">
                     Esto es lo que opinan nuestros usuarios:
                 </h3>
-                <div className="border border-gray-300 rounded-lg mb-4"></div>
-                <div className="flex flex-col items-end gap-3">
+                {/* <div className="border border-gray-300 rounded-lg mb-4 w-[99vw]"></div> */}
+                <div className="flex flex-col items-end gap-3 border border-cyan-400 rounded-xl mx-8 p-2 w-[90vw] sm:max-w-xl">
                     <button
-                        className="text-white px-2 py-2 mr-2 rounded-lg bg-[#009EB5] hover:bg-[#009ec3]"
+                        className="text-white px-2 py-2 mr-3 rounded-lg bg-[#009EB5] hover:bg-[#009ec3]"
                         onClick={() => navigate('/assessments')}
                     >
                         Dejar una valoracion
                     </button>
-                    <button className=" py-2 px-5 mr-2 text-white bg-[#009EB5] border rounded-md shadow-md hover:bg-[#009ec3]">
+                    <button className="py-2 px-5 mr-3 text-white bg-[#009EB5] border rounded-lg shadow-md hover:bg-[#009ec3]">
                         Ver valoraciones
                     </button>
                 </div>
