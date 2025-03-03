8701a3d409dc05e5610f4dfbc157bc507bbe4401
javi.garrido <fjgarridovalderas@hotmail.com>
2025-03-03 21:23:06 +0100

    Footer modificado

--- src/components/LayoutPublic/Footer.jsx
+++ src/components/LayoutPublic/Footer.jsx
@@ -9,13 +9,13 @@
 //     );
 // };
 
-import { Logo } from '../Logo.jsx';
+import { Logofooter } from '../LogoFooter.jsx';
 
 export const Footer = () => {
     return (
-        <footer className="flex flex-col gap-2 items-center justify-center w-full bg-gradient-to-r from-cyan-300 via-teal-300 to-slate-900 p-2 sm:p-4 text-white text-xs sm:text-sm md:text-base">
+        <footer className="flex flex-col gap-2 items-center justify-center w-full bg-black p-2 sm:p-4 text-white text-xs sm:text-sm md:text-base">
             {/* Logo ajustable según el tamaño de la pantalla */}
-            <Logo className="w-16 sm:w-20 md:w-24 mb-2" />
+            <Logofooter className="w-16 sm:w-20 md:w-24 mb-2" />
             <p className="text-center">Copyright© Hackloud</p>
         </footer>
     );
---
+++ src/components/LogoFooter.jsx
@@ -0,0 +1,18 @@
+import logoFooter from '../assets/img/logo-hackloud-solo-blanco.png';
+import { Link } from 'react-router-dom';
+import { useAuthHook } from '../hooks/useAuthHook.js';
+
+export const Logofooter = () => {
+    const { currentUser } = useAuthHook();
+    const redirectPath = currentUser ? '/storage' : '/';
+
+    return (
+        <Link to={redirectPath}>
+            <img
+                src={logoFooter}
+                alt="Hackloud blanco"
+                className="h-4 w-auto hover:opacity-80 transition-opacity md:h-6"
+            />
+        </Link>
+    );
+};
