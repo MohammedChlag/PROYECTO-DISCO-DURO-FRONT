3cec28434b18c40cec41e5d0021ae57943f56419
Sebas Osuna <sebasosunah@gmail.com>
2025-03-09 17:40:58 +0100

    sidebar

---
+++ src/components/layout/AcctionSidebarButton.jsx
@@ -0,0 +1,16 @@
+export const ActionSidebarButton = ({ icon: Icon, label, onClick }) => {
+    return (
+        <button
+            onClick={onClick}
+            className="
+          flex items-center w-full px-4 py-3 
+          bg-[#009EB5] text-white rounded-lg
+          hover:bg-[#009ec3] transition-colors duration-200
+          mb-2
+        "
+        >
+            <Icon className="h-5 w-5 mr-2" />
+            <span>{label}</span>
+        </button>
+    );
+};
