255feca40035d1493360f35d7028d93e7edb0f9b
javi.garrido <fjgarridovalderas@hotmail.com>
2025-03-10 21:44:05 +0100

    boton mode noche posicion movil

--- src/components/ThemeUserMode.jsx
+++ src/components/ThemeUserMode.jsx
@@ -1,4 +1,3 @@
-
 import { MoonIcon as MoonSolid, SunIcon } from '@heroicons/react/24/solid';
 import React, { useEffect, useState } from 'react';
 
@@ -24,32 +23,37 @@ export const ThemeUserMode = () => {
 
     const handleChangeTheme = () => {
         setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-        setIsChecked((prev) => !prev)
+        setIsChecked((prev) => !prev);
     };
 
     return (
-        <div className="absolute top-2 right-6 z-50">
-          <button
-            onClick={handleChangeTheme}
-            className={`relative inline-flex h-8 w-14 items-center rounded-full ${
-              theme === 'dark' ? 'bg-gray-600' : 'bg-[#009EB5]'
-            } dark:bg-[#676767]`}
-            role="switch"
-            aria-checked={isChecked}
-          >
-            <span className="sr-only">Theme mode</span>
-            <span
-              className={`inline-block h-6 w-6 transform rounded-full bg-white transition duration-200 ease-in-out ${
-                theme === 'dark' ? 'translate-x-7 flex items-center justify-center' : 'translate-x-1 flex items-center justify-center'
-              }`}
+        <div className="absolute top-3 left-[30vw] sm:top-4 z-50">
+            <button
+                onClick={handleChangeTheme}
+                className={`relative inline-flex h-6 w-12 items-center rounded-full ${
+                    theme === 'dark' ? 'bg-gray-600' : 'bg-[#009EB5]'
+                } dark:bg-[#676767]`}
+                role="switch"
+                aria-checked={isChecked}
             >
-              {theme === 'dark' ? (
-                <SunIcon className="size-4 fill-yellow-500" aria-hidden="true" />
-              ) : (
-                <MoonSolid className="size-4" aria-hidden="true" />
-              )}
-            </span>
-          </button>
+                <span className="sr-only">Theme mode</span>
+                <span
+                    className={`inline-block h-5 w-5 transform rounded-full bg-white transition duration-200 ease-in-out ${
+                        theme === 'dark'
+                            ? 'translate-x-7 flex items-center justify-center'
+                            : 'translate-x-1 flex items-center justify-center'
+                    }`}
+                >
+                    {theme === 'dark' ? (
+                        <SunIcon
+                            className="size-4 fill-yellow-500"
+                            aria-hidden="true"
+                        />
+                    ) : (
+                        <MoonSolid className="size-4" aria-hidden="true" />
+                    )}
+                </span>
+            </button>
         </div>
-      );
-    };
+    );
+};
