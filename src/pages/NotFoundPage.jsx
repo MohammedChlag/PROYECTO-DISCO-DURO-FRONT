9bdad5fb2b002de3ae39da3486ffb93d5973a24c
Bridinia Aguero <bridiniaaguero@gmail.com>
2025-03-10 21:20:06 +0100

    arreglitos modo noche

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,14 +7,14 @@ export const NotFoundPage = () => {
     return (
         <>
             <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat">
-                <div className="max-w-md mx-auto text-center bg-white bg-opacity-90 p-4">
+                <div className="max-w-md mx-auto text-center bg-white dark:bg-[#1f1f1f] bg-opacity-90 p-4">
                     <div className="text-8xl font-bold text-cyan-400 mb-4">
                         404
                     </div>
-                    <h1 className="text-4xl font-bold text-gray-800 mb-6">
+                    <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-6">
                         Oops! Página no encontrada
                     </h1>
-                    <p className="text-lg text-gray-600 mb-8">
+                    <p className="text-lg text-gray-600 dark:text-gray-400 dar mb-8">
                         Esta página está más perdida que el One Piece... Pero no
                         te preocupes, te devolvemos de vuelta en un click!.
                     </p>
